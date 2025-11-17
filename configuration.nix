dab06502e0a522a77d68a12a5ab5fb8f337b0069
VladBlnO0 <vladpuku@gmail.com>
2025-11-17 19:43:17 +0200

    firewall for frontend, backend, db

--- configuration.nix
+++ configuration.nix
@@ -79,7 +79,7 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 80 443 22 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 22 3000 3306 4000];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
